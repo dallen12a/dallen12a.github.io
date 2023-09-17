9bd1871466e521661c948d2e4a600dc768fd11c9
dallen12a <107562574+dallen12a@users.noreply.github.com>
2023-09-17 12:05:22 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+function toggleSection(sectionId) {
+  const sections = document.querySelectorAll('.content');
+
+  sections.forEach(section => {
+    section.style.display = 'none';
+  });
+
+  const selectedSection = document.getElementById(sectionId);
+  selectedSection.style.display = 'block';
+}
+
 function toggleDarkMode() {
   document.body.classList.toggle('dark-mode');
 }
