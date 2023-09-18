833a47090c2f8de96eb49045bdee322867e35b91
dallen12a <107562574+dallen12a@users.noreply.github.com>
2023-09-18 14:32:29 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,28 @@
-function toggleSection(sectionId) {
-  const sections = document.querySelectorAll('.content');
-
-  sections.forEach(section => {
-    section.style.display = 'none';
-  });
-
-  const selectedSection = document.getElementById(sectionId);
-  selectedSection.style.display = 'block';
-}
-
+// Toggle between light and dark mode
 function toggleDarkMode() {
-  document.body.classList.toggle('dark-mode');
+  const body = document.body;
+  body.classList.toggle('dark-mode');
 }
 
+// Smooth scrolling
+function scrollToSection(sectionId) {
+  const section = document.getElementById(sectionId);
+  window.scrollTo({
+    top: section.offsetTop,
+    behavior: 'smooth'
+  });
+}
 
-document.addEventListener('DOMContentLoaded', type);
+// Set up event listener for the toggle button
+const toggleButton = document.getElementById('toggle-button');
+toggleButton.addEventListener('click', toggleDarkMode);
+
+// Set up event listeners for navigation links
+const navLinks = document.querySelectorAll('nav a');
+navLinks.forEach(link => {
+  link.addEventListener('click', (event) => {
+    event.preventDefault();
+    const targetSectionId = link.getAttribute('href').substring(1);
+    scrollToSection(targetSectionId);
+  });
+});
