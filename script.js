2f74488590d4ef2815bc4f9d12dd0d9524b77279
dallen12a <107562574+dallen12a@users.noreply.github.com>
2023-09-19 00:13:05 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,4 @@
-function hoverProject(element) {
-  element.style.boxShadow = '0 0 10px rgba(255, 255, 255, 0.5)';
+function toggleTheme() {
+  const body = document.body;
+  body.classList.toggle('dark-theme');
 }
-
-function unhoverProject(element) {
-  element.style.boxShadow = 'none';
-}
-
-function toggleDescription(project) {
-  const description = project.querySelector('.description');
-  description.classList.toggle('show');
-}
-
-// Add waving hand animation
-const profilePicture = document.querySelector('.profile-picture');
-profilePicture.classList.add('waving-hand');
