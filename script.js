096f7d12d6580b9c0dd7c9c46cd0b91ed1749c0e
dallen12a <107562574+dallen12a@users.noreply.github.com>
2023-09-18 23:33:21 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,28 @@
 function hoverProject(element) {
-  element.style.boxShadow = '0 0 10px rgba(0, 0, 0, 0.2)';
+  element.style.boxShadow = '0 0 10px rgba(255, 255, 255, 0.5)';
 }
 
 function unhoverProject(element) {
   element.style.boxShadow = 'none';
+}
+
+function toggleDescription(project) {
+  const description = project.querySelector('.description');
+  description.classList.toggle('show');
+}
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function(e) {
+    e.preventDefault();
+
+    const targetId = this.getAttribute('href').substring(1);
+    const targetElement = document.getElementById(targetId);
+
+    if (targetElement) {
+      window.scrollTo({
+        top: targetElement.offsetTop,
+        behavior: 'smooth'
+      });
+    }
+  });
 }
