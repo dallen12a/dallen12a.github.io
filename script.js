c57dc0ccf6b83b6ae18eab88d03c1b804153a39e
dallen12a <107562574+dallen12a@users.noreply.github.com>
2023-09-18 23:47:08 -0400

    Update script.js

--- script.js
+++ script.js
@@ -11,18 +11,6 @@ function toggleDescription(project) {
   description.classList.toggle('show');
 }
 
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-  anchor.addEventListener('click', function(e) {
-    e.preventDefault();
-
-    const targetId = this.getAttribute('href').substring(1);
-    const targetElement = document.getElementById(targetId);
-
-    if (targetElement) {
-      window.scrollTo({
-        top: targetElement.offsetTop,
-        behavior: 'smooth'
-      });
-    }
-  });
-}
+// Add waving hand animation
+const profilePicture = document.querySelector('.profile-picture');
+profilePicture.classList.add('waving-hand');
