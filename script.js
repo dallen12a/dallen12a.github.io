bd22ce04d026fc4478e12c50fc7f285231a613e3
dallen12a <107562574+dallen12a@users.noreply.github.com>
2023-09-19 07:50:29 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 function toggleTheme() {
   const body = document.body;
   const container = document.querySelector('.container');
-  
+  const profile = document.querySelector('.profile');
+  const projects = document.querySelector('.projects');
+  const projectCards = document.querySelectorAll('.project-card');
+
   body.classList.toggle('dark-theme');
   container.classList.toggle('dark-theme');
+  profile.classList.toggle('dark-theme');
+  projects.classList.toggle('dark-theme');
+
+  projectCards.forEach(card => {
+    card.classList.toggle('dark-theme');
+  });
 }
