2ce61237e72aa6a558e53c6fe49780b80473f639
dallen12a <107562574+dallen12a@users.noreply.github.com>
2023-09-18 23:18:18 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,7 @@
-let index = 0;
-const typingText = "Welcome to Dave's Portfolio";
-
-function type() {
-  const typingDiv = document.getElementById('typing-text');
-  if (index < typingText.length) {
-    typingDiv.innerHTML += typingText.charAt(index);
-    index++;
-    setTimeout(type, 50); // Adjust the delay for faster typing effect
-  }
+function hoverProject(element) {
+  element.style.boxShadow = '0 0 10px rgba(0, 0, 0, 0.2)';
 }
 
-function toggleDarkMode() {
-  const body = document.body;
-  const toggleButton = document.getElementById('toggle-button');
-  body.classList.toggle('dark-mode');
-  toggleButton.innerText = body.classList.contains('dark-mode') ? '☀️ Light Mode' : '🌙 Dark Mode';
+function unhoverProject(element) {
+  element.style.boxShadow = 'none';
 }
-
-// Set up event listener for the toggle button
-const toggleButton = document.getElementById('toggle-button');
-toggleButton.addEventListener('click', toggleDarkMode);
-
-// Start typing effect after a short delay
-setTimeout(type, 1000); // Adjust the delay before starting typing effect
