994de4bc4ffc21dd4604f8053da5c7b45a309b0b
dallen12a <107562574+dallen12a@users.noreply.github.com>
2023-09-19 07:11:16 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 function toggleTheme() {
   const body = document.body;
+  const container = document.querySelector('.container');
+  
   body.classList.toggle('dark-theme');
+  container.classList.toggle('dark-theme');
 }
